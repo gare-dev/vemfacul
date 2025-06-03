69ee5a767ab8316a72df5dabffc1097678c47185
anndrem <dealmeida.andre08@gmail.com>
2025-06-03 13:10:27 -0300

    refractor: redefinir senha

--- pages/alterarSenha/[email].tsx
+++ pages/alterarSenha/[email].tsx
@@ -1,28 +1,32 @@
 import Api from "@/api";
 import Styles from "@/styles/alterarSenha.module.scss";
+import { verify } from "crypto";
 import { useRouter } from "next/router";
 
 const alterarSenha = () => {
     const router = useRouter();
+    const {email} = router.query;
+
+    if (!email || typeof email !== 'string') {
+        return <div>Carregando...</div>;
+    }
     return (
+
         <div className={Styles.container}>
             <div className={Styles.form}>
                 <h1>Redefinindo Senha</h1>
                 <form onSubmit={async (e) => {
                     e.preventDefault();
                     const password = (e.target as HTMLFormElement).password.value;
-                    const email = (e.target as HTMLFormElement).email.value;
+                    console.log("Enviando para resetPassword:", { password, email });
                     try {
                         await Api.resetPassword(password, email);
                         alert('Senha redefinida com sucesso.');
                         router.push('/');
-                    } catch (error) {
-                        alert('Erro ao redefinir senha. Tente novamente.');
+                    } catch (error: any) {
+                        alert(`Error ao redefinir senha: ${error?.response?.data?.message || error.message || error}`);
                     }
-
                 }}>
-                    <p className={Styles.label}>CONFIRME O EMAIL</p>
-                    <input type="email" name="email" required />
                     <p className={Styles.label}>NOVA SENHA</p>
                     <input type="password" name="password" autoComplete="off" required />
                     <div className={Styles.buttonDiv}>
