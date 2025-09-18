027e8459584bf099ef7fd4b966b4548852f875d3
Guilherme Gare <ggare.contato@gmail.com>
2025-09-18 08:26:03 -0300

    feat: adicionado tempo relativo nos posts

--- types/coursetype.ts
+++ types/coursetype.ts
@@ -1,17 +1,6 @@
 // types/course.ts
 export interface Course {
-    // id_cursinho: string
-    // nome: string
-    // nome_exibido: string
-    // cidade: string
-    // uf: string
-    // faixa_preco: string
-    // logo: string
-    // media: string
-    // total_avaliacoes: string
-    // representante_legal: string
-    // email_contato: string
-    // telefone: string
+
     id_cursinho: string;
     nome: string;
     nome_exibido: string;
