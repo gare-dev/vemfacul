2f733d21b09cc242c214b223a7ae9d6911777de5
Guilherme Gare <ggare.contato@gmail.com>
2025-09-22 17:51:48 -0300

    fix

--- pages/redacao/index.tsx
+++ pages/redacao/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "@/styles/redacao.module.scss";
 import Api from "@/api";
 import { GetServerSideProps } from "next";
