e8a23d353515de8e80615814da2472a3111a8f73
anndrem <dealmeida.andre08@gmail.com>
2025-09-22 20:21:58 -0300

    feat: side bar com notificações

--- pages/Notificacoes/index.tsx
+++ pages/Notificacoes/index.tsx
@@ -1,7 +1,43 @@
+import Api from "@/api";
 import Notification from "@/components/Notification";
+import Sidebar from "@/components/Sidebar"
+import AuthDataType from "@/types/authDataType";
+import { GetServerSideProps } from "next";
+import { useState } from "react";
 
-export default function Main() {
+interface Props {
+    authData?: AuthDataType | null | undefined;
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+    try {
+        const cookie = ctx.req.headers.cookie
+        Api.setCookie(cookie || "")
+
+        const authData = await Api.getProfileInfo()
+
+        return {
+            props: {
+                authData: authData.data.code === "PROFILE_INFO" ? authData.data.data : null
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            props: {
+                authData: null
+            }
+        }
+    }
+}
+
+export default function Main({ authData }: Props) {
+    const [userInfo, setUserInfo] = useState<string[]>([])
+
     return (
-        <Notification />
+        <>
+            <Sidebar setInfo={setUserInfo} userInfo={userInfo} authData={authData} />
+            <Notification />
+        </>
     )
 }
